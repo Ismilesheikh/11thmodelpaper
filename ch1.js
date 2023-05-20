181f8da785fcfa7dbbc0ee3f76c97a79c39eb875
Ismilesheikh <ismiledeveloper@gmail.com>
2023-05-20 23:34:11 +0530

    op

--- ch1.js
+++ ch1.js
@@ -1,7 +1,73 @@
+//coded by Ismile sheikh//
+var seen = "Answer Seen"
+a1_1.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'red';
+  event.target.style.color = 'white';
+});
+b1_1.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'green';
+  event.target.style.color = 'white';
+});
+c1_1.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'red';
+  event.target.style.color = 'white';
+});
+d1_1.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'red';
+  event.target.style.color = 'white';
+});
+e1_1.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'green';
+  event.target.style.color = 'white';
+b1_1.style.backgroundColor = 'green';
+b1_1.style.color = 'white';
+e1_1.innerHTML = seen;
+});
 
+a1_2.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'red';
+  event.target.style.color = 'white';
+});
+b1_2.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'red';
+  event.target.style.color = 'white';
+});
+c1_2.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'green';
+  event.target.style.color = 'white';
+});
+d1_2.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'red';
+  event.target.style.color = 'white';
+});
+e1_2.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'green';
+  event.target.style.color = 'white';
+c1_2.style.backgroundColor = 'green';
+c1_2.style.color = 'white';
+e1_2.innerHTML = seen;
+});
 
-
-var btn = document.getElementById('btn');
-
-btn.addEventListener( 'click' , function onClick(event)
-{event.target.style.backgroundColor = 'red';});
+a1_3.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'red';
+  event.target.style.color = 'white';
+});
+b1_3.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'red';
+  event.target.style.color = 'white';
+});
+c1_3.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'green';
+  event.target.style.color = 'white';
+});
+d1_3.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'red';
+  event.target.style.color = 'white';
+});
+e1_3.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'green';
+  event.target.style.color = 'white';
+c1_3.style.backgroundColor = 'green';
+c1_3.style.color = 'white';
+e1_3.innerHTML = seen;
+});
