a41b49c11d49794df4923d0d48df1635948b2ac2
Ismilesheikh <ismiledeveloper@gmail.com>
2023-05-21 11:59:08 +0530

    ok then

--- ch1.js
+++ ch1.js
@@ -71,3 +71,189 @@ c1_3.style.backgroundColor = 'green';
 c1_3.style.color = 'white';
 e1_3.innerHTML = seen;
 });
+
+//4//
+
+a1_4.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'red';
+  event.target.style.color = 'white';
+});
+b1_4.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'red';
+  event.target.style.color = 'white';
+});
+c1_4.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'red';
+  event.target.style.color = 'white';
+});
+d1_4.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'green';
+  event.target.style.color = 'white';
+});
+e1_4.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'green';
+  event.target.style.color = 'white';
+d1_4.style.backgroundColor = 'green';
+d1_4.style.color = 'white';
+e1_4.innerHTML = seen;
+});
+
+
+//5//
+
+
+a1_5.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'red';
+  event.target.style.color = 'white';
+});
+b1_5.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'red';
+  event.target.style.color = 'white';
+});
+c1_5.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'red';
+  event.target.style.color = 'white';
+});
+d1_5.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'green';
+  event.target.style.color = 'white';
+});
+e1_5.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'green';
+  event.target.style.color = 'white';
+d1_5.style.backgroundColor = 'green';
+d1_5.style.color = 'white';
+e1_5.innerHTML = seen;
+});
+
+//6//
+
+a1_6.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'green';
+  event.target.style.color = 'white';
+});
+b1_6.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'red';
+  event.target.style.color = 'white';
+});
+c1_6.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'red';
+  event.target.style.color = 'white';
+});
+d1_6.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'red';
+  event.target.style.color = 'white';
+});
+e1_6.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'green';
+  event.target.style.color = 'white';
+a1_6.style.backgroundColor = 'green';
+a1_6.style.color = 'white';
+e1_6.innerHTML = seen;
+});
+
+//7//
+
+a1_7.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'red';
+  event.target.style.color = 'white';
+});
+b1_7.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'green';
+  event.target.style.color = 'white';
+});
+c1_7.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'red';
+  event.target.style.color = 'white';
+});
+d1_7.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'red';
+  event.target.style.color = 'white';
+});
+e1_7.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'green';
+  event.target.style.color = 'white';
+b1_7.style.backgroundColor = 'green';
+b1_7.style.color = 'white';
+e1_7.innerHTML = seen;
+});
+
+//8//
+
+a1_8.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'red';
+  event.target.style.color = 'white';
+});
+b1_8.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'red';
+  event.target.style.color = 'white';
+});
+c1_8.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'green';
+  event.target.style.color = 'white';
+});
+d1_8.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'red';
+  event.target.style.color = 'white';
+});
+e1_8.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'green';
+  event.target.style.color = 'white';
+c1_8.style.backgroundColor = 'green';
+c1_8.style.color = 'white';
+e1_8.innerHTML = seen;
+});
+
+//9//
+
+a1_9.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'green';
+  event.target.style.color = 'white';
+});
+b1_9.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'red';
+  event.target.style.color = 'white';
+});
+c1_9.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'red';
+  event.target.style.color = 'white';
+});
+d1_9.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'red';
+  event.target.style.color = 'white';
+});
+e1_9.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'green';
+  event.target.style.color = 'white';
+a1_9.style.backgroundColor = 'green';
+a1_9.style.color = 'white';
+e1_9.innerHTML = seen;
+});
+
+
+//10//
+
+a1_10.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'red';
+  event.target.style.color = 'white';
+});
+b1_10.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'green';
+  event.target.style.color = 'white';
+});
+c1_10.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'red';
+  event.target.style.color = 'white';
+});
+d1_10.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'red';
+  event.target.style.color = 'white';
+});
+e1_10.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'green';
+  event.target.style.color = 'white';
+b1_10.style.backgroundColor = 'green';
+b1_10.style.color = 'white';
+e1_10.innerHTML = seen;
+});
+
